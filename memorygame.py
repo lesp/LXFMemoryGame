0aa02da48c62ffc68bdbb2079a9318f87d017295
Les Pounder <lespounder@gmail.com>
2017-01-02 12:17:40 +0000

    first commit

---
+++ memorygame.py
@@ -0,0 +1,75 @@
+import time
+import random
+from gpiozero import LED, Button
+
+red = LED(2)
+yellow = LED(3)
+green = LED(4)
+blue = LED(17)
+
+red_button = Button(14)
+yellow_button = Button(15)
+green_button = Button(18)
+blue_button = Button(23)
+
+leds = ["red","yellow","green","blue"]
+
+for led in leds:
+    led.on()
+    time.sleep(0.2)
+    led.off()
+    time.sleep(0.2)
+
+
+while True:
+    red_button.wait_for_press()
+    print("Standby for light sequence!!!")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+    print("GO!!!")
+    sequence = random.shuffle(leds)
+    #Printing the LED sequence to the Python shell is used for debugging
+    print(sequence)
+    for led in sequence:
+        led.on()
+        time.sleep(0.1)
+        led.off()
+    answer = []
+    while len(answer) <4:
+        time.sleep(0.3)
+        if red_button.pressed():
+            answer.append("red")
+            red.on()
+            time.sleep(0.1)
+            red.off()
+            print(answer)
+        elif yellow_button.is_pressed():
+            answer.append("yellow")
+            yellow.on()
+            time.sleep(0.1)
+            yellow.off()
+            print(answer)
+        elif green_button.is_pressed():
+            answer.append("green")
+            green.on()
+            time.sleep(0.1)
+            green.off()
+            print(answer)
+        elif blue_button.is_pressed():
+            answer.append("blue")
+            blue.on()
+            time.sleep(0.1)
+            blue.off()
+            print(answer)
+    if sequence == answer:
+        print("YOU WIN!!!"
+        for led in leds:
+            led.blink(on_time=0.2,off_time=0.2)
+        time.sleep(5)
+        for led in leds:
+            led.off()
